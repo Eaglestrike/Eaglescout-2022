935e98a96833e3aaa898f9440567e1c2c02fcd08
unknown <100023295@mvla.net>
2022-03-22 11:22:52 -0700

    added new ranking formats

--- config/multipliers.js
+++ config/multipliers.js
@@ -1,18 +1,19 @@
 module.exports = {
 	multipliers: {
-		'auto_low_goals': 0,
-		'auto_high_goals': 0,
-		'teleop_low_goals': 0,
-		'teleop_high_goals': 0,
-		'shoot_launch_pad': 0,
-		'shoot_tarmac': 0,
-		'shoot_terminal': 0,
-		'shoot_wherever': 0,
-		'speeds': 0,
-		'defense': 0,
-		'climb_level': 0,
-		'climb_time': 0,
-		'climb_fail': 0,
-		'death_percent': 0
+		'auto_low_goals': 100,
+		'auto_high_goals': 100,
+		'teleop_low_goals': 100,
+		'teleop_high_goals': 100,
+		'shoot_launch_pad': 100,
+		'shoot_tarmac': 100,
+		'shoot_terminal': 100,
+		'shoot_wherever': 100,
+		'speeds': 100,
+		'defense': 100,
+		'driver_skill': 100,
+		'climb_level': 100,
+		'climb_time': 100,
+		'climb_fail': 100,
+		'death_percent': 100
 	}
 };
---
+++ config/summary.js
@@ -0,0 +1,25 @@
+module.exports = { 
+    capabilities: {
+        'auto_taxi': {'yes': 1, 'no': 0},
+        'auto_low_goals': 'number',
+        'auto_high_goals': 'number',
+        'teleop_low_goals': 'number',
+        'teleop_high_goals': 'number',
+        'teleop_eject_balls': 'string_arr',
+        'teleop_shoot_balls': 'string_arr',
+        //0 is slow, 1 is medium, 2 is fast
+        'speed': {'slow': 0, 'medium': 1, 'fast': 2},
+        //climb is 0 is none, 1 is low, 2 is mid, 3 is high, 4 is traversal
+        'endgame_climb': {'low_bar': 1, 'mid_bar': 2, 'high_bar': 3, 'traverse_bar': 4,'no_attempt': 0, 'failed': -1},
+        'teleop_robot_died': 'match_list',
+        'time_on_defense': 'match_list',
+    },
+    gamePoints: {
+        'auto_taxi': {'yes': 2, 'no': 0},
+        'auto_low_goals': 2,
+        'auto_high_goals': 4,
+        'teleop_low_goals': 1,
+        'teleop_high_goals': 2,
+        'endgame_climb': {'low_bar': 4, 'mid_bar': 6, 'high_bar': 10, 'traverse_bar': 15,'no_attempt': 0, 'failed': 0},
+    }
+};
