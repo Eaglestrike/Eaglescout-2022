8035acfd7eb73ccb075479c4b61fae6e2387b99b
Gabor Szita <gaborszita88@gmail.com>
2022-11-16 18:29:10 -0800

    Migrate from express-validator v5 to v6

--- routes/account.js
+++ routes/account.js
@@ -2,20 +2,21 @@ var express = require('express');
 var router = express.Router();
 var utils = require("../utils");
 var User = require("../models/user");
+const { check, validationResult } = require('express-validator');
 
 router.get('/', utils.ensureAuthenticated, function(req, res) {
 	res.render('account');
 });
 
-router.post('/', utils.ensureAuthenticated, function(req, res) {
+router.post('/', utils.ensureAuthenticated, async function(req, res) {
 	var oldPassword = req.body.oldPassword;
 	var newPassword = req.body.newPassword;
 	var confirmPassword = req.body.confirmPassword;
-	req.checkBody('newPassword', 'Please enter a password!').notEmpty();
-	req.checkBody('confirmPassword', 'Passwords do not match.').equals(req.body.newPassword);
+	await check('newPassword', 'Please enter a password!').notEmpty().run(req);
+	await check('confirmPassword', 'Passwords do not match.').equals(req.body.newPassword).run(req);
 
-	var errors = req.validationErrors();
-	if (errors) {
+	var errors = validationResult(req);
+	if (!errors.isEmpty()) {
 		req.flash('error_msg', 'Passwords do not match.');
 		res.redirect('/account');
 	} else {
